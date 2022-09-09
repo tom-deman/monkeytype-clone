ad4aa66c98e2c39caae54e09e0ec1f9c206e49e5
Thomas <dt.deman.thomas@gmail.com>
2022-09-09 17:03:32 +0200

    project functionnality done, style to do

--- src/App.jsx
+++ src/App.jsx
@@ -7,16 +7,70 @@ import randomWords from 'random-words'
 
 
 const App = () => {
-    const nbrWords = 100
+    const nbrWords = 55
 
-    const [ userInput, setUserInput       ] = useState( []                                              )
-    const [ words, setWords               ] = useState( randomWords( nbrWords ).join( ' ' ).split( '' ) )
-    const [ initialTimer, setInitialTimer ] = useState( 30                                              )
-    const [ timer, setTimer               ] = useState( initialTimer                                    )
-    const [ start, setStart               ] = useState( false                                           )
-    const [ wpm, setWpm                   ] = useState( undefined                                       )
+    const [ userInput, setUserInput       ] = useState( []                       )
+    const [ initialWords, setInitialWords ] = useState( randomWords( nbrWords )  )
+    const [ initialTimer, setInitialTimer ] = useState( 30                       )
+    const [ timer, setTimer               ] = useState( initialTimer             )
+    const [ start, setStart               ] = useState( false                    )
+    const [ wpm, setWpm                   ] = useState( undefined                )
+    const [ wordIndex, setWordIndex       ] = useState( 0                        )
+    const [ letterIndex, setLetterIndex   ] = useState( 0                        )
+    const [ length, setLength             ] = useState( initialWords[ 0 ].length )
+    const [ counter, setCounter           ] = useState( 0                        )
+    const [ fullCounter, setFullCounter   ] = useState( 0                        )
 
-    const allowedKey = [ 8, 9, 32, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 186, 187, 188, 189, 219, 220, 221, 222 ]
+    const allowedKey = [
+        8,
+        9,
+        32,
+        48,
+        49,
+        50,
+        51,
+        52,
+        53,
+        54,
+        55,
+        56,
+        57,
+        65,
+        66,
+        67,
+        68,
+        69,
+        70,
+        71,
+        72,
+        73,
+        74,
+        75,
+        76,
+        77,
+        78,
+        79,
+        80,
+        81,
+        82,
+        83,
+        84,
+        85,
+        86,
+        87,
+        88,
+        89,
+        90,
+        186,
+        187,
+        188,
+        189,
+        219,
+        220,
+        221,
+        222
+    ]
+
     const timerButtons = [
         15,
         30,
@@ -25,35 +79,71 @@ const App = () => {
     ]
 
 
+    const restart = () => {
+        setInitialWords( randomWords( nbrWords )  )
+        setInitialTimer( initialTimer             )
+        setUserInput   ( []                       )
+        setStart       ( false                    )
+        setTimer       ( initialTimer             )
+        setWpm         ( undefined                )
+        setLength      ( initialWords[ 0 ].length )
+        setWordIndex   ( 0                        )
+        setLetterIndex ( 0                        )
+        setCounter     ( 0                        )
+        setFullCounter ( 0                        )
+    }
+
     const handleUserInput = ( event ) => {
         if( allowedKey.includes( event.which ) ) {
             if( event.which === 8 ) {
-                const newTab = [ ...userInput ]
-                newTab.splice( userInput.length - 1, 1 )
-                setUserInput( [ ...newTab ] )
+                if( letterIndex === 0 ) return
+
+                else {
+                    const newTab = [ ...userInput ]
+
+                    newTab.splice ( userInput.length - 1, 1    )
+                    setUserInput  ( [ ...newTab ]              )
+                    setLetterIndex( prevState => prevState - 1 )
+                    setCounter    ( counter - 1                )
+                    setFullCounter( fullCounter - 1            )
+                }
             }
+
             else if( event.which === 9 ) {
                 event.preventDefault()
-                setWords( randomWords( nbrWords ).join( ' ' ).split( '' ) )
-                setUserInput( [] )
-                setStart( false )
-                setInitialTimer( 10 )
-                setTimer( initialTimer )
-                setWpm( undefined )
+                restart()
             }
+
             else {
-                setStart( true )
-                if( event.key === words[ userInput.length ] ) {
+                setStart      ( true            )
+                setCounter    ( counter + 1     )
+                setFullCounter( fullCounter + 1 )
+                setLetterIndex( letterIndex + 1 )
+
+                if( counter > 135 ) {
+                    setCounter     ( 0                                                  )
+                    setInitialWords( [ ...initialWords, randomWords( 20 ).join( ' ' ) ] )
+                }
+
+                if( event.key === initialWords.join( ' ' ).split( '' )[ userInput.length ] ) {
                     setUserInput( [ ...userInput, {
                         letter: event.key,
-                        valid: true
+                        valid : true,
+                        key   : wordIndex.toString() + letterIndex.toString()
                     } ] )
                 }
+
                 else{
                     setUserInput( [ ...userInput, {
                         letter: event.key,
-                        valid: false
+                        valid : false,
+                        key   : wordIndex.toString() + letterIndex.toString()
                     } ] )
+                }
+
+                if( letterIndex === length ) {
+                    setLetterIndex( 0             )
+                    setWordIndex  ( wordIndex + 1 )
                 }
             }
         }
@@ -63,7 +153,7 @@ const App = () => {
 
     useEffect( () => {
         document.addEventListener( 'keydown', handleUserInput )
-        return () => { document.removeEventListener( 'keydown', handleUserInput ) }
+        return () => document.removeEventListener( 'keydown', handleUserInput )
     }, [ userInput ] )
 
     useEffect( () => {
@@ -87,44 +177,95 @@ const App = () => {
         setTimer( initialTimer )
     }, [ initialTimer ] )
 
+    useEffect( () => {
+        const myLength = initialWords[ wordIndex ].length
+        setLength( myLength )
+    }, [ wordIndex ] )
+
 
     return(
-        <div style={{ padding: '50px' }}>
-            { words.map( ( el, index ) => {
-                return(
-                    <span
-                        key={ index }
-                        style={{
-                            color: !userInput[ index ]
-                                ? 'gray'
-                                : userInput[ index ] && userInput[ index ].valid
-                                    ? 'green'
-                                    : 'red',
-                            display: 'inline-block',
-                            fontSize: '22px'
-                        }}
-                    >
-                        { el === ' ' ? '\u00a0' : el }
-                    </span>
-                )
-            } ) }
-            <p>
-                { timer }
-                { wpm && wpm }
-            </p>
-            { timerButtons.map( ( el, index ) => <button
-                onClick={ () => setInitialTimer( el ) }
-                key={ index }
+        <>
+        { !wpm
+            ? <div
+                style={ {
+                    padding  : '50px',
+                    textAlign: 'center',
+                    position : 'relative'
+                } }
             >
-                { el }
-            </button> ) }
-        </div>
+                { initialWords.map( ( el, i ) => {
+                    return(
+                        <span
+                            key={ i }
+                            style={ {
+                                whiteSpace: 'nowrap',
+                                display   : 'inline-block'
+                            } }
+                        >
+                            { el.split( '' ).concat( ' ' ).map( ( letter, j ) => {
+                                return(
+                                    <span
+                                        key={ j }
+                                        style={{
+                                            display : 'inline-block',
+                                            fontSize: '22px',
+                                            position: 'relative',
+                                            color   : !userInput.find( el => el.key === ( i.toString() + j.toString() ) )
+                                                ? 'gray'
+                                                : userInput.find( el => el.key === ( i.toString() + j.toString() ) ) &&
+                                                userInput.find( el => el.key === ( i.toString() + j.toString() ) ).valid
+                                                    ? 'green'
+                                                    : 'red'
+                                        }}
+                                    >
+                                        { letter === ' ' ? '\u00a0' : letter }
+                                        <div
+                                            style={{
+                                                position       : 'absolute',
+                                                height         : '100%',
+                                                width          : '1px',
+                                                backgroundColor: 'blue',
+                                                top            : '0',
+                                                right          : '0',
+                                                display        :
+                                                    userInput.find(
+                                                        el => el.key === ( i.toString() + j.toString()
+                                                    ) ) &&
+                                                    userInput.find(
+                                                        el => el.key === ( i.toString() + j.toString()
+                                                    ) ).key === userInput[ fullCounter - 1 ].key
+                                                        ? 'block'
+                                                        : 'none'
+                                            }}
+                                        >
+                                        </div>
+                                    </span>
+                                )
+                            } ) }
+                        </span>
+                    )
+                } ) }
+                <p>
+                    { timer }
+                </p>
+                { timerButtons.map( ( el, index ) => (
+                    <button
+                        onClick={ () => setInitialTimer( el ) }
+                        key    ={ index                       }
+                    >
+                        { el }
+                    </button>
+                ) ) }
+                <button onClick={ () => restart() }>
+                    Restart
+                </button>
+            </div>
+            : <div>
+                { wpm }
+            </div>
+        }
+        </>
     )
 }
 
 export default App
-
-// Split letter into words to break at end of width
-// Add cursor to help vizualise where we are in the application
-// Add only more words if needed
-// Do style app
